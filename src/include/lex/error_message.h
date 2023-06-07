3c1ce76dea829972f0d8013f5a960c3e8811d643
zdowney926 <10782580+zdowney926@user.noreply.gitee.com>
2023-06-07 11:17:41 +0800

    define ErrorMessage class

--- src/include/lex/error_message.h
+++ src/include/lex/error_message.h
@@ -5,13 +5,19 @@
 #ifndef GREAT_BUCT_COMPILER_ERRORMSG_H
 #define GREAT_BUCT_COMPILER_ERRORMSG_H
 
-#endif //GREAT_BUCT_COMPILER_ERRORMSG_H
+#include<string>
+
 namespace gbc {
 namespace lex {
 
 class ErrorMessage {
-
+public:
+    static void ReportError(const std::string& message);
+    static void ReportWarning(const std::string& message);
 };
 
 } // lex
 } // gbc
+
+
+#endif //GREAT_BUCT_COMPILER_ERRORMSG_H
