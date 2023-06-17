ff6a4bbc5478330512853a8c1a9958beb77fae0a
Eric2i <eric2ihsiung@gmail.com>
2023-06-17 18:08:45 +0800

    Update CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@
 
 #include "test/lex/regex_test.cc"
 #include "test/lex/NFA_test.cc"
+#include "test/lex/DFA_test.cc"
+#include "test/lex/analyzer_test.cc"
 
 #include <fstream>
 #include <iostream>
 
 int main() {
-    gbc::lex::nfa_tester();
+//    LexicalRulesTester();
+//    gbc::lex::nfa_tester();
+//    gbc::lex::rules_to_dfa_tester();
+    gbc::lex::lexical_analyzer_tester();
     return 0;
 }
